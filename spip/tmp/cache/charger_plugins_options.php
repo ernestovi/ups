ac627a4d346fcb66cfacea941b99d2fa3d089d58
ernestovi <villazonluis@gmail.com>
2016-03-16 22:27:20 +0100

    cms+lms
    ajout des dossiers spip et moodle

---
+++ spip/tmp/cache/charger_plugins_options.php
@@ -0,0 +1,9 @@
+<?php
+
+if (defined('_ECRIRE_INC_VERSION')) {
+define('_PLUGINS_HASH','9f6eda0f472546988cb2102b0231b427');
+include_once_check(_ROOT_PLUGINS_DIST.'petitions/petitions_options.php');
+include_once_check(_ROOT_PLUGINS_DIST.'squelettes_par_rubrique/squelettes_par_rubrique_options.php');
+include_once_check(_ROOT_PLUGINS_DIST.'compresseur/compresseur_options.php');
+}
+?>
