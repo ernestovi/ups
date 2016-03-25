3679a44a4dbc64e773a43b15a4369a857fba2b4c
noëlno <nadine@acer>
2016-03-25 18:35:14 +0100

    Base de données via XMLDB
    install.xml installe les nouvelles tables

--- profileplus/version.php
+++ profileplus/version.php
@@ -1,15 +1,19 @@
-<?php // $Id: version.php,v 1.5.2.2 2009/03/19 12:23:11 mudrd8mz Exp $
+<?php
 
 /**
- * Code fragment to define the version of profileplus
- * This fragment is called by moodle_needs_upgrading() and /admin/index.php
+ * Fragment de code définissant la version de profileplus
  *
- * @author  Your Name <your@email.address>
- * @version $Id: version.php,v 1.5.2.2 2009/03/19 12:23:11 mudrd8mz Exp $
- * @package mod/profileplus
+ * @author  Nadine Noël <n.noel@arml-idf.org>
+ * @package local/profileplus
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2007040200;  // The current module version (Date: YYYYMMDDXX)
-$module->cron     = 0;           // Period for cron to check this module (secs)
+defined('MOODLE_INTERNAL') || die(); // empêche l'accès direct à ce fichier par le navigateur. https://moodle.org/mod/forum/discuss.php?d=227737
+ 
+$plugin->version  = 2015032500;  // syntaxe requise : AAAAMMJJXX, AAAAMMJJ étant la date de finalisation de la version et XX étant 00, à incrémenter seulement si plusieurs versions sortent le même
+$plugin->component = 'local_profileplus'; // syntaxe requise : 'typedeplugin_nomduplugin'
+$plugin->requires = 2015111600; // numéro de version de Moodle requise pour l'installation, ici Moodle 3.0. Les numéros de versions sont ici : https://docs.moodle.org/dev/Releases)
+$plugin->maturity = MATURITY_ALPHA; // (A MODIFIER) indique si la version est stable ou s'il s'agit d'une version alpha, beta ou rc
+$plugin->release = 'v1'; // numéro de version lu par les utilisateurs humains
 
 ?>
