597bb45769004c6fd9a23f77306b6488a0c8796f
wumiolabisi <wumi.olabisi@gmail.com>
2016-05-18 15:55:20 +0200

    Adaptation du menu pour la page d'accueil en résolution 1280px
    Tout est dit dans le titre

--- spip/squelettes/themeups/js/frontFunctions.js
+++ spip/squelettes/themeups/js/frontFunctions.js
@@ -5,17 +5,34 @@ $( document ).ready(function() {
 	//Open menu
 	$("#iconMenu").click(function(){
 		
-		if( screen.width =< 800 ){
+		if( screen.width <= 800 ){
 			
 			document.getElementById("menuSlide").style.display = "inline-block";
 			document.getElementById("pageContent").style.display = "none";
 
 		}
 		
-		else if( screen.width >= 1280 ){
+		else if( screen.width > 800 ){
 			
 			document.getElementById("menuSlide").style.display = "inline-block";
-			document.getElementById("pageContent").style.display = "none";
+			
+			document.getElementById("iconMenu").style.display = "none";
+			
+			document.getElementById("textLogo").style.marginLeft = "200px";
+			document.getElementById("textLogo").style.marginRight = "240px";
+			
+			document.getElementById("iconUser").style.marginLeft = "0px";
+			
+			var lineHeader3 = document.getElementById("lineHeader3");
+			lineHeader3.getElementsByTagName('a')[0].style.marginLeft = "800px";
+
+			document.getElementById("lineHeader4").style.marginLeft = "1130px";
+			
+			document.getElementById("popularCourse").style.marginLeft = "200px";
+			
+			var popularCourseTitle = document.getElementById("popularCourse");
+			popularCourseTitle.getElementsByClassName("title")[0].style.width = "900px";
+
 
 		}
 
@@ -23,10 +40,36 @@ $( document ).ready(function() {
 	
 	$("#closeMenu").click(function(){
 		
-		if( screen.width < 800 ){
+		if( screen.width <= 800 ){
 			
 			document.getElementById("menuSlide").style.display = "none";
 			document.getElementById("pageContent").style.display = "inline";
+
+		}
+		
+		else if( screen.width > 800 ){
+			
+			document.getElementById("menuSlide").style.display = "none";
+			document.getElementById("iconMenu").style.display = "inline-block";
+			
+			/* Reset changed CSS values */
+			
+			
+			document.getElementById("textLogo").style.marginLeft = null;
+			document.getElementById("textLogo").style.marginRight = null;
+			
+			document.getElementById("iconUser").style.marginLeft = null;
+			
+			var lineHeader3 = document.getElementById("lineHeader3");
+			lineHeader3.getElementsByTagName('a')[0].style.marginLeft = null;
+
+			document.getElementById("lineHeader4").style.marginLeft = null;
+			
+			document.getElementById("popularCourse").style.marginLeft = null;
+			
+			var popularCourseTitle = document.getElementById("popularCourse");
+			popularCourseTitle.getElementsByClassName("title")[0].style.width = null;
+
 
 		}
 	});
